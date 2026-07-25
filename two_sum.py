2d217a2902d92c6a6b178676f8eba76bbe69032b
Emeka Ndulue <iamemekandulue@gmail.com>
2026-07-25 18:26:03 +0100

    first commit

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        for i, n in enumerate(nums):
+            difference = target - n
+            if difference in hashMap:
+                return [hashMap[difference], i]
+            hashMap[difference] = i
+        return []
+    
+    
