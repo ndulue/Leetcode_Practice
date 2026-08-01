dd0444651109f6d1415e46b2b6e700c1de8cb61b
Emeka Ndulue <iamemekandulue@gmail.com>
2026-08-01 18:40:51 +0100

    Onesan

--- Decode_ways.py
+++ Decode_ways.py
@@ -11,7 +11,7 @@ class Solution:
             
             res = dfs(i + 1)
             
-            if (i + 1 < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] in '0123456'))):
+            if (i + 1 < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] in "0123456"))):
                 res += dfs(i + 2)
             dp[i] = res
             return res
