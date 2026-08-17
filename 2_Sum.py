0e9c3d1b8c9856cc697f286765b7eae740d209c6
Emeka Ndulue <iamemekandulue@gmail.com>
2026-08-17 18:16:54 +0100

    Hima

---
+++ 2_Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        if len(nums) <= 0:
+            return False
+        
+        dict1 = {}
+        
+        for i in range(len(nums)):
+            
+            if nums[i] in dict1:
+                return dict1[nums[i]], i
+            
+            else:
+                dict1[target - nums[i]] = i 
