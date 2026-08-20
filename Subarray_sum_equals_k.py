4e4df204aa2c94a17dd547e17889b2e95246a599
Emeka Ndulue <iamemekandulue@gmail.com>
2026-08-20 18:23:21 +0100

    Maee

---
+++ Subarray_sum_equals_k.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        if len(nums) <= 0:
+            return 0
+        
+        sumdict = {0:1}
+        n = len(nums)
+        count = 0
+        s = 0
+        
+        for num in nums:
+            s += num
+            
+            if s - k in sumdict:
+                count += sumdict[s-k]
+            
+            if s in sumdict:
+                sumdict[s] += 1
+            else:
+                sumdict[s] = 1
+        return count
+    
+    
