e2813e28a42568f8f623978233f2dfcad6be046c
Emeka Ndulue <iamemekandulue@gmail.com>
2026-08-24 19:10:15 +0100

    Tenki

---
+++ Permutation_in_string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkinclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        length_of_s1 = len(s1)
+        s1_counter = Counter(s1)
+        window_counter = Counter()
+        
+        for i, c in enumerate(s2):
+            window_counter[c] += 1
+            
+            if i >= length_of_s1:
+                element_from_left = s2[i - length_of_s1]
+            
+            if window_counter[element_from_left] == 1:
+                del window_counter[element_from_left]
+            else:
+                window_counter[element_from_left] -= 1
+            
+            if s1_counter == window_counter:
+                return True
+            
+            
+        return False
