22182bf85e271c224f41367cd476a4f2fea91dfa
Emeka Ndulue <iamemekandulue@gmail.com>
2026-08-25 18:17:24 +0100

    Esoede

--- Symmetric_Tree.py
+++ Symmetric_Tree.py
@@ -6,7 +6,7 @@ class Solution:
         return self.isMirror(root.left, root.right)
     
     
-    def isMirror(Lroot: TreeNode, Rroot: TreeNode) -> bool:
+    def isMirror(self, Lroot: TreeNode, Rroot: TreeNode) -> bool:
         if Lroot and Rroot:
             return Lroot.val == Rroot.val and self.isMirror(Lroot.right, Rroot.right) and self.isMirror(Lroot.left, Rroot.left)
         
